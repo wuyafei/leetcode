05e95448b91f5cdb7436379e428df1da977a7630
wuyafei <wuyafei0406@gmail.com>
2015-01-01 19:26:53 +0800

    path sum II, this recursive solution solved the TLE error

--- leetcode/leetcode/pathSum2.cpp
+++ leetcode/leetcode/pathSum2.cpp
@@ -2,27 +2,48 @@
 #include<vector>
 #include<stack>
 using namespace std;
-vector<vector<int> > pathSum(TreeNode* root, int sum){
+
+//this solution get the time limit exceed error
+//vector<vector<int> > pathSum2(TreeNode* root, int sum){
+//	vector<vector<int> > rslt;
+//	if (!root)
+//		return rslt;
+//	stack<pair<TreeNode*, int> > stk;
+//	vector<int> vec;
+//	stk.push(make_pair(root, sum));
+//	while (!stk.empty()){
+//		pair<TreeNode*, int> p = stk.top();
+//		stk.pop();
+//		vec.push_back(p.second);
+//		if (p.second == p.first->val && !p.first->left && !p.first->right){
+//			rslt.push_back(vec);
+//			vec.pop_back();
+//		}
+//		if (p.first->right){
+//			stk.push(make_pair(p.first->right, p.second - p.first->val));
+//		}
+//		if (p.first->left){
+//			stk.push(make_pair(p.first->left, p.second - p.first->val));
+//		}
+//	}
+//	return rslt;
+//}
+
+vector<vector<int> > pathSum2(TreeNode* root, int sum){
 	vector<vector<int> > rslt;
-	if (!root)
-		return rslt;
-	stack<pair<TreeNode*, int> > stk;
 	vector<int> vec;
-	stk.push(make_pair(root, sum));
-	while (!stk.empty()){
-		pair<TreeNode*, int> p = stk.top();
-		stk.pop();
-		vec.push_back(p.second);
-		if (p.second == p.first->val && !p.first->left && !p.first->right){
-			rslt.push_back(vec);
-			vec.pop_back();
-		}
-		if (p.first->right){
-			stk.push(make_pair(p.first->right, p.second - p.first->val));
-		}
-		if (p.first->left){
-			stk.push(make_pair(p.first->left, p.second - p.first->val));
-		}
-	}
+	pathDown(root, sum, rslt, vec);
 	return rslt;
+}
+
+void pathDown(TreeNode* node, int target, vector<vector<int> > &rslt, vector<int> vec){
+	if (!node)
+		return;
+	vec.push_back(node->val);
+	if (node->val == target && !node->left && !node->right){
+		rslt.push_back(vec);
+		return;
+	}
+	pathDown(node->left, target - node->val, rslt, vec);
+	pathDown(node->right, target - node->val, rslt, vec);
 }
