2e77f3df2e5d8d0b180042f4872eef4589bec403
wuyafei <wuyafei0406@gmail.com>
2015-01-03 13:02:08 +0800

    more compact solution of remove duplicates

--- leetcode/leetcode/removeDuplicates.cpp
+++ leetcode/leetcode/removeDuplicates.cpp
@@ -7,20 +7,12 @@ Do not allocate extra space for another array, you must do this in place with co
 int removeDuplicates(int A[], int n){
 	if (n == 0)
 		return 0;
-	int i = 1, j = 1;
-	while (j < n && i < n){
-		if (A[i]>A[i - 1]){
-			i++;
-			continue;
-		}
-		while (j < n && A[j] <= A[i-1]){
+	int i = 0, j = 1;
+	while (j < n){
+		if (A[i] == A[j])
 			j++;
-		}
-		if (j == n)
-			return i;
-		A[i] = A[j];
-		i++;
-		j++;
+		else
+			A[++i] = A[j++];
 	}
-	return i;
+	return i+1;
 }
